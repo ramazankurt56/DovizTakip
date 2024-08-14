27af0c1bc1ce7eae4faee0800ee93ea88bb5a701
ramazankurt56 <93058427+ramazankurt56@users.noreply.github.com>
2024-08-14 12:38:51 +0300

    first commit

---
+++ DovizTakipServer/DovizTakipServer.Application/Features/Currencies/CreateCurrency/CreateCurrencyCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using DovizTakipServer.Application.Hubs;
+using DovizTakipServer.Domain.Entities;
+using DovizTakipServer.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using Microsoft.AspNetCore.SignalR;
+using TS.Result;
+
+namespace DovizTakipServer.Application.Features.Currencies.CreateCurrency;
+
+internal sealed class CreateCurrencyCommandHandler(
+    ICurrencyRepository currencyRepository,
+    IHubContext<TakipHub> hubContext,
+    IUnitOfWork unitOfWork) : IRequestHandler<CreateCurrencyCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(CreateCurrencyCommand request, CancellationToken cancellationToken)
+    {
+        Currency currency = Currency.Create(request.Amount, request.TypeValue);
+
+        await currencyRepository.AddAsync(currency, cancellationToken);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        await hubContext.Clients.All.SendAsync("Currency", currency);
+
+        return "Create is successful";
+    }
+}
